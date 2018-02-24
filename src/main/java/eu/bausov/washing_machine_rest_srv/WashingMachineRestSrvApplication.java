2b50e87aaeb6c2ead645e0092e9b75e077ecf761
GreenNun <stas909@gmail.com>
2018-02-24 17:10:53 +0300

    initial commit

---
+++ src/main/java/eu/bausov/washing_machine_rest_srv/WashingMachineRestSrvApplication.java
@@ -0,0 +1,12 @@
+package eu.bausov.washing_machine_rest_srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashingMachineRestSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashingMachineRestSrvApplication.class, args);
+	}
+}
---
+++ src/test/java/eu/bausov/washing_machine_rest_srv/WashingMachineRestSrvApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.bausov.washing_machine_rest_srv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WashingMachineRestSrvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
